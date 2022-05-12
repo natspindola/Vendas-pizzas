dc0c1c3481a2dc8d3c37721a66e46b56d083bb50
Perk <natspindola@hotmail.com>
2022-05-12 20:50:25 -0300

    função estorno

--- javascript.js
+++ javascript.js
@@ -36,3 +36,37 @@ function venda(e) {
         document.getElementById('vendaCalabresa').innerHTML = vendaCalabresa.toFixed(2);
     }
 }
+
+function estorno(e) {
+    if (document.getElementById('mussarela').checked) {
+        somaMussarela = somaMussarela - 1;
+        vendaMussarela = vendaMussarela - 25.00;
+        if(somaMussarela < 0) 
+        {
+            somaMussarela = 0;
+            vendaMussarela = 0.00;
+        }
+        document.getElementById('resultadoMussarela').innerHTML = somaMussarela;
+        document.getElementById('vendaMussarela').innerHTML = vendaMussarela.toFixed(2);
+    } else if (document.getElementById('marguerita').checked) {
+        somaMarguerita = somaMarguerita - 1;
+        vendaMarguerita = vendaMarguerita - 25.00;
+        if(somaMussarela < 0) 
+        {
+            somaMarguerita = 0;
+            vendaMarguerita = 0.00;
+        }
+        document.getElementById('resultadoMarguerita').innerHTML = somaMarguerita;
+        document.getElementById('vendaMarguerita').innerHTML = vendaMarguerita.toFixed(2);
+    } else if (document.getElementById('calabresa').checked) {
+        somaCalabresa = somaCalabresa - 1;
+        vendaCalabresa = vendaCalabresa - 25.00;
+        if(somaMussarela < 0) 
+        {
+            somaCalabresa = 0;
+            vendaCalabresa = 0.00;
+        }
+        document.getElementById('resultadoCalabresa').innerHTML = somaCalabresa;
+        document.getElementById('vendaCalabresa').innerHTML = vendaCalabresa.toFixed(2);
+    }
+}
