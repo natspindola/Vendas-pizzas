31abf208bd00dd660d4c825e06b4c6d93da4a125
Perk <natspindola@hotmail.com>
2022-05-12 20:06:32 -0300

    variáveis

---
+++ javascript.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// variáveis
+let somaMussarela = 0;
+let somaMarguerita = 0;
+let somaCalabresa = 0;
+let vendaMussarela = 0.00;
+let vendaMarguerita = 0.00;
+let vendaCalabresa = 0.00;
+
+let btnVendas = document.getElementById('vendas');
+let btnEstorno = document.getAnimations('estorno');
+let btnPlanilha = document.getElementById('export');
