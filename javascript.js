a473fead6d8e9a2753436d6d0c92c000490e4123
Perk <natspindola@hotmail.com>
2022-05-12 20:20:08 -0300

    eventos e funções

--- javascript.js
+++ javascript.js
@@ -11,3 +11,24 @@ let vendaCalabresa = 0.00;
 let btnVendas = document.getElementById('vendas');
 let btnEstorno = document.getAnimations('estorno');
 let btnPlanilha = document.getElementById('export');
+
+// eventos
+btnVendas.addEventListener('click', venda, false);
+btnEstorno.addEventListener('click', estorno, false);
+btnPlanilha.addEventListener('click', planilha, false);
+
+// funções
+function venda(e)
+{
+    if(document.getElementById('mussarela').checked)
+    {
+        somaMussarela = somaMussarela + 1;
+        vendaMussarela = vendaMussarela + 25.00;
+    } else if (document.getElementById('marguerita').checked) {
+        somaMarguerita = somaMarguerita + 1;
+        vendaMussarela = vendaMussarela + 25.00;
+    } else if (document.getElementById('calabresa').checked) {
+        somaCalabresa = somaCalabresa + 1;
+        vendaCalabresa = vendaCalabresa + 25.00;
+    }
+}
